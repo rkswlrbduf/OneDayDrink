20455ba18950d83d3285d7777f0471b07671cdf3
KyuYeol <rkswlrbduf@gmail.com>
2018-05-20 21:31:27 +0900

    second

---
+++ app/src/main/java/com/example/kyuyeol/onedaydrink/RecyclerViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.kyuyeol.onedaydrink
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class RecyclerViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+
+
+}
